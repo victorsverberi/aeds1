23a1e15a6d7943de16b7f31cb519749e1780f1fa
Victor Sverberi Maia <v.sverberi@gmail.com>
2024-02-26 12:08:55 -0300

    Add files via upload

---
+++ aula1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.14159
+
+int main(void) {
+
+	int idade = 26;
+	const int MAX_NUM = 100;
+
+	printf("minha variavel int comum = %d\n", idade);
+	printf("minha variavel int constante = %d\n", MAX_NUM);
+	printf("minha constante = %f\n", PI);
+
+
+
+
+
+
+	return EXIT_SUCCESS;
+}
